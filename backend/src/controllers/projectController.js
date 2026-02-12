d98ae6b213853970a167bdafad57a87b676da9e4
digiabhi <singhabhishek111213@gmail.com>
2026-02-12 12:29:25 +0530

    Added backend api to add new projects

---
+++ backend/src/controllers/projectController.js
@@ -0,0 +1,18 @@
+import util from 'util';
+import child_process from 'child_process';
+import fs from 'fs/promises';
+import uuid4 from 'uuid4';
+
+const execPromisified = util.promisify(child_process.exec);
+
+export const createProjectController = async(req, res) => {
+    const projectId = uuid4();
+
+    await fs.mkdir(`./projects/${projectId}`);
+
+    const response = await execPromisified(`npm create vite@latest sandbox -- --template react`, {
+        cwd: `./projects/${projectId}`
+    });
+
+    return res.json({message: "Project created successfully!", data: projectId})
+}
